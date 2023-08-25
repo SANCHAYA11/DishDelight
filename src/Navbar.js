4bfb0a93d79739fb1f140ccf099e8672933148b2
SANCHAYA11 <66821486+SANCHAYA11@users.noreply.github.com>
2023-08-25 23:30:40 +0530

    commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Navbar(){
     return <div style={{backgroundColor:'teal'}}>
-        <h1>React Meals</h1>
+        <h1>DishDelight</h1>
         <a style={{margin:'10px',color:'white'}}>Home</a>
         <a style={{margin:'10px',color:'white'}}>  Order</a>
         <a style={{margin:'10px',color:'white'}}>Contact Us</a>
